c1c3c21fdc27904efd2e033059fa4759f48664e8
prlanzarin <prlanzarin@inf.ufrgs.br>
2017-09-28 16:05:51 +0000

    First mcs-core draft

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# mcs-core
+Copyright © 2016 [Mconf Tecnologia].
+
+Installing npm and Node.js
+====================
+```bash
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+```
+
+Update npm
+==
+
+```bash
+sudo npm install npm -g
+```
+
+
+Install dependencies
+==
+```bash
+npm install
+```
+
+Run
+==
+```bash
+npm start
+```
+Run the server (for mosaic):
+
+    $ node server.js
---
+++ server.js
@@ -0,0 +1,19 @@
+var config = require('config');
+var mcs = require('./lib/media/MCSApiStub.js');
+
+function stopAndExit(error) {
+  var exitTime = config.get('kurento.exit_timeout');
+
+  setTimeout( function() { process.exit(1); }, exitTime );
+}
+
+process.stdin.resume();
+
+process.on('SIGTERM', stopAndExit);
+process.on('SIGINT', stopAndExit);
+
+process.on('uncaughtException', function(e)
+{
+    console.log('[uncaughtException] app should be terminated: ', e.stack);
+    //stopAndExit(e);
+});
