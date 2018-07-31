655c196e343257032043c9cc82b72e45ee22f323
prlanzarin <prlanzarin@inf.ufrgs.br>
2018-07-31 16:30:33 +0000

    mcs-js compatible version with FS adapter, recording, error handling and standalone deployment

--- CoreProcess.js
+++ CoreProcess.js
@@ -1,12 +1,50 @@
-const MCSApiStub = require('./media/MCSApiStub');
+/*
+ * WIP: the mcs-core lib will be turned into a dependecy sometime in the near
+ * future, and it will probably act with a separate process that answers via
+ * its own redis channel
+ */
 
-process.on('uncaughtException', function (error) {
-  console.log(error.stack);
+'use strict';
+const Logger = require('../utils/Logger');
+const MCSApiStub = require('./lib/media/MCSApiStub');
+const API = require('mcs-js');
+const http = require('http');
+const config = require('config');
+
+let controller = new MCSApiStub();
+
+Logger.info('[app] MCS Server is initializing ...');
+
+// HTTPS server
+let port = config.get('mcs-port');
+let serverHttps = http.createServer().listen(port, () => {
+  Logger.info('[app] MCS Server is ready to receive connections');
 });
 
-process.on('disconnect',function() {
-  console.log("Parent exited!");
-  process.kill();
+let mcsServer = new API.Server({
+  path: config.get('mcs-path'),
+  server: serverHttps,
+  connectionTimeout: 1000
 });
 
-core = new MCSApiStub();
+mcsServer.on('connection', (client) => {
+  Logger.info('[app] Client connected');
+  controller.setupClient(client);
+});
+
+let exit = () => {
+  process.exit();
+}
+
+process.on('SIGINT', () => {
+  exit();
+});
+
+process.on('uncaughtException', (e) => {
+  Logger.error(e);
+});
+
+process.on('unhandledrejection', (e1, e2) => {
+  Logger.error(e1,e2);
+});
+
